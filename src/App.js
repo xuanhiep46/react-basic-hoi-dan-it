a1a1960578010f262813911b9deac73dcaec734b
xuanhiep46 <xuanhiep0881888@gmail.com>
2024-08-28 18:27:11 +0700

    form in ReactJS - Thu  thập dữ liệu Input HTML

--- src/App.js
+++ src/App.js
@@ -18,14 +18,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hello world with React.js(Hoi Dan IT)</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
           <MyComponent/>
           {/* link nó tới trang MyComponent */}
         </header>
--- src/Example/MyComponent.js
+++ src/Example/MyComponent.js
@@ -2,54 +2,61 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
-    state = { //là một Object
-        name: '',
-        channel: 'xunhiep'
+    state = {
+        firstName: '',
+        lastName: '',
     }
 
-    handleOnChangeName = (event) => {
-        //this.state.name = event.target.value; đây là bad code
-        //k bao giờ gọi state trực tiếp giống bên trên mà hãy dùng cái hàm gọi state giống bên dưới -> đi lâu dài
-        //merge
-        this.setState({
-            name: event.target.value
+    handleChangeFirstName = (event) => {
+        this.setState ({ //dùng hàm để cập nhật state
+            firstName: event.target.value //lấy giá trị của 'event'.truy cập đến thuộc tính input(dùng target).lấy giá trị của thằng 'value'
         })
     }
 
-    handleClickButton = () => {
-        alert('Click me')
+    handleChangeLastName = (event) => {
+        this.state ({
+            lasteName: event.target.value
+        })
     }
 
-    //re-render
-    render() { //ở trong class thì function kh cần khai báo function
-        
-        let name = 'hiep';
+    handleSubmit = (event) => {
+        event.preventDefault() //tránh refresh lại trang
+        console.log('>>> check data input',this.state) //state đang chứa data
+    }
 
-        return( //phải bao tất cả trong 1 lớp bọc 'dev', đây là nguyên tắc của JSX
-            <React.Fragment>  {/* thay cho việc bạn phải học thẻ div bên ngoài */}
-                <div className="first">
-                    <input value={this.state.name} type='text'
-                        onChange={(event) => this.handleOnChangeName(event)}
+    render() {
+        console.log('>>> call render: ',this.state)
+
+        return(
+            //React.Fragment - dấu <> </> bên dưới là viết tắt của <React.Fragment></React.Fragment>
+            <> 
+                <form>
+                    <label htmlFor="fname">First name:</label><br/>
+                    <input 
+                    type="text" 
+                    value={this.state.firstName} //cập nhật giá trị real-time cho ô firstName
+                    onChange={(event) => this.handleChangeFirstName(event)} //gọi State và dùng onChange để thay đổi giá trị trực tiếp
                     />
-                </div>
-
-                <div className="second">
-                hello my again, my name is { this.state.name }, and my youtube channel is {this.state.channel} 
-                {/* this = MyComponent(tên class). state(object). name(pros của thằng object) */}
-                </div>
-
-                <div className="third">
-                    <button onClick={ () => this.handleClickButton() }>Click me</button> 
-                    {/* phải dùng adofunction thì mới gọi nó được */}
-                </div>
-            </React.Fragment>
+                    <br/> 
+                    <label htmlFor="lname">Last name:</label><br/>
+                    <input 
+                    type="text" 
+                    value={this.state.lastName}
+                    onChange={(event) => this.handleChangeLastName(event)}
+                    />
+                    <br/><br/>
+                    <input type="submit"
+                        onClick={(event) => this.handleSubmit(event)} //chạy khi bạn bấm nút
+                    />
+                </form> 
+            </>
         )
         
     }
 }
 
-export default MyComponent; //trả về MyComponent
+export default MyComponent;
 
 //Hãy learn by doing, không biết thì gg =))
 
-//đã xong bài 7
+//đã xong bài 9 ở phút thứ 25:00
